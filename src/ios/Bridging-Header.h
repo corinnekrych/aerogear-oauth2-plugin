1fe7da8e51a53c040966a22dd6ca6c565c9e6877
Erik Jan de Wit <erikjan.dewit@gmail.com>
2014-11-11 11:46:13 +0100

    change oath2 plugin to be pure javascript

--- src/ios/Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Bridging-Header.h
-//  HelloCordova
-//
-//  Created by Erik Jan de Wit on 28/10/14.
-//
-//
-
-#import <Cordova/CDV.h>
--- src/ios/OAuth2Plugin.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
- @objc(OAuth2Plugin) class OAuth2Plugin : CDVPlugin {
-    func getGoogleDriveFiles(command: CDVInvokedUrlCommand) {
-        var pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
-        commandDelegate.sendPluginResult(pluginResult, callbackId:command.callbackId)
-    }
-}
